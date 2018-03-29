f6c7469431427d8df26a3a059ec091175ce32aa1
Debanjan Datta <ddatta@vt.edu>
2018-03-29 12:58:06 -0400

    starting off the wavent for ts

--- model_4.py
+++ model_4.py
@@ -1,27 +1,95 @@
-import tensorflow as tf
-import sklearn
+import keras.layers as layers
+import keras.models as models
+import keras
 import numpy as np
-
+import data_feeder
+import utils
 
 
 class model:
 
     def __init__(self):
+        self.model = None
+        self.build()
         return
 
-    def get_conv_layer(stride):
-        tf.nn.conv1d(
-            value,
-            filters,
-            stride,
-            padding,
-            use_cudnn_on_gpu=None,
-            data_format=None,
-            name=None
-        )
     def set_hyperparameters(self):
+        self.batch_size = 128
+        self.time_window = 25
         return
+
+
+    def set_train_data(self, train_x, train_y):
+        self.train_x = train_x
+        self.train_y = train_y
+        return
+
+
+    def res_block(self, inp, num_filters, kernel_size):
+        # down-sampling is performed with a stride of 2
+        conv_layer_op = layers.Conv1D(filters=num_filters,
+                                      kernel_size=kernel_size,
+                                      strides=[1],
+                                      padding='same')(inp)
+
+        res = layers.BatchNormalization()(conv_layer_op)
+        res = layers.LeakyReLU()(res)
+        res = layers.add([res, inp])
+        return res
+
 
     def build(self):
+        inp_data = np.random.rand(10, 10, 1)
+        # input is an image of 10 x 1
+
+        input_layer = layers.Input(shape=(10, 1))
+        network_op = self.res_block(
+            inp=input_layer,
+            num_filters=4,
+            kernel_size=[3]
+        )
+        model = models.Model(
+            input = [input_layer],
+            outputs = [network_op]
+        )
+        model.compile(
+            optimizer = keras.optimizers.Adam(),
+            loss = keras.losses.MSE,
+            metrics = [keras.metrics.mae, keras.metrics.mse]
+        )
+
+        print model.summary()
         return
 
+
+    def train_model(self):
+        self.error_keys = {
+            'mse': 'mean_squared_error',
+            'mae': 'mean_absolute_error',
+        }
+        self.train_losses = []
+        self.train_mape = []
+        self.train_mse = []
+        self.train_mae = []
+        _x = self.train_x
+        _y = self.train_y
+        bs =100
+
+        hist = self.model.fit(
+            _x,
+            _y,
+            epochs=10,
+            batch_size=bs,
+            shuffle=False,
+            verbose=False
+        )
+
+        self.train_losses.extend(hist.history['loss'])
+        self.train_mse.extend(hist.history[self.error_keys['mse']])
+        self.train_mae.extend(hist.history[self.error_keys['mae']])
+        return
+
+
+model()
+
+# ---------------- #
--- utils.py
+++ utils.py
@@ -1,6 +1,27 @@
 import matplotlib.pyplot as plt
+from itertools import tee, izip
+
 
 def plot(x,y):
     # plt.xticks(range(min(x), max(x) + 1, 1000))
     plt.plot(x, y, 'r-.')
     plt.show()
+
+
+# window the exogenous input
+def get_windowed_data(data, window_size):
+    # local function
+    def window(iterable, size):
+        iters = tee(iterable, size)
+        for i in xrange(1, size):
+            for each in iters[i:]:
+                next(each, None)
+        return izip(*iters)
+
+    op = []
+    for w in window(data, window_size):
+        w = np.reshape(w, [-1])
+        op.append(w)
+
+    op = np.asarray(op)
+    return op
