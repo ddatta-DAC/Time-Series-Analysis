626bfcde80c6c63119f2edc2d31885f8d0f23db4
Debanjan Datta <ddatta@vt.edu>
2018-05-01 16:38:37 -0400

    updated model_5

--- data_set1_models_new/model_5.py
+++ data_set1_models_new/model_5.py
@@ -7,6 +7,8 @@ from itertools import tee, izip
 import data_feeder
 import os
 from keras.models import load_model
+import pprint
+
 
 def get_windowed_data(data, window_size):
     # local function
@@ -26,6 +28,12 @@ def get_windowed_data(data, window_size):
     op = np.asarray(op)
     return op
 
+# ------------------------------ #
+exog_dim  = 5
+encoder_units = [16,8]
+decoder_units = [16,8]
+
+# ------------------------------ #
 
 class encoder_decoder_model:
 
@@ -34,13 +42,16 @@ class encoder_decoder_model:
         self.model = None
         return
 
-    def set_hyperparams(self,
-                        exg_dimension=81,
-                        end_window_size=16,
-                        encoder_layer_units=[32, 32], decoder_layer_units=[32, 16],
-                        epochs=4,
-                        lstm_time_steps=8,
-                        batch_size=128):
+    def set_hyperparams(
+            self,
+            exg_dimension,
+            end_window_size,
+            encoder_layer_units,
+            decoder_layer_units,
+            epochs,
+            lstm_time_steps,
+            batch_size
+    ):
 
         self.exg_dimension = exg_dimension
         self.encoder_layer_units = encoder_layer_units
@@ -59,7 +70,9 @@ class encoder_decoder_model:
             # return
 
         # First input layer for the Network
-        input_layer_1 = layers.Input(shape=(self.lstm_time_steps, self.exg_dimension))
+        input_layer_1 = layers.Input(
+            shape=(self.lstm_time_steps, self.exg_dimension)
+        )
         current_op = input_layer_1
         num_encoder_layers = len(self.encoder_layer_units)
         for l in range(num_encoder_layers):
@@ -67,14 +80,19 @@ class encoder_decoder_model:
                 inp = input_layer_1
                 lstm_inp_dim = self.exg_dimension
 
+
             op = layers.LSTM(
                 units=self.encoder_layer_units[l],
-                input_shape=(self.lstm_time_steps, lstm_inp_dim),
+                input_shape=(
+                    self.lstm_time_steps,
+                    lstm_inp_dim
+                ),
                 activation='tanh',
                 return_sequences=True
             )(inp)
 
             inp = op
+            # For next layer
             lstm_inp_dim = self.encoder_layer_units[l]
             current_op = op
 
@@ -151,41 +169,91 @@ class encoder_decoder_model:
 
     def train_model(self):
 
-        exog_train, _, end_train, _, _ = data_feeder.get_data(True)
+        exog_train, _, _, end_train, _, _ , _ = data_feeder.get_data_val(True)
         x1, x2, y = self.format_data_xy(exog_train, end_train)
         hist = self.model.fit([x1,x2],y,epochs=self.epochs)
-        print hist.history['loss']
-        # self.model.save(self.model_file)
-        return
+        trian_loss = hist.history['loss']
+        return np.mean(trian_loss)
+
+
+    def val_model(self):
+        _, exog_val, _,  _, end_val, _, _ = data_feeder.get_data_val(True)
+        x1, x2, y = self.format_data_xy(exog_val, end_val)
+        score = self.model.evaluate([x1,x2],y)
+        val_mse = score[0]
+        return val_mse
 
 
     def test_model(self):
-        _, exog_test,  _, end_test, _ = data_feeder.get_data(True)
+
+        _, _, exog_test, _, _, end_test, _ = data_feeder.get_data_val(True)
         x1, x2, y = self.format_data_xy(exog_test, end_test)
-        print self.model.evaluate([x1,x2],y)
+        score = self.model.evaluate([x1,x2],y)
+        test_mse = score[0]
+        return test_mse
 
 # ------------------------------- #
 
+import pandas as pd
 
-model_obj = encoder_decoder_model()
+def experiment():
 
-exg_dimension = 81
-end_window_size = 16
-encoder_layer_units = [16 , 16]
-decoder_layer_units = [16, 16]
-epochs = 50
-lstm_time_steps = 16
-batch_size = 128
+    exg_dimension = 81
+    end_window_size = 16
+    encoder_layer_units = [16, 8]
+    decoder_layer_units = [16, 8]
+    epochs = 350
+    lstm_time_steps = 16
+    batch_size = 128
 
-model_obj.set_hyperparams(
-    exg_dimension,
-    end_window_size,
-    encoder_layer_units,
-    decoder_layer_units,
-    epochs,
-    lstm_time_steps,
-    batch_size
-)
-model_obj.build_model()
-model_obj.train_model()
-model_obj.test_model()
+    _enc_units = [[16, 8], [16, 16], [32, 16]]
+    _dec_units = [[16, 8], [16, 16], [32, 16]]
+    ts = [16, 32, 64]
+    columns = ['End Window Size',
+               'Encoder Layer 1 Units',
+               'Encoder Layer 2 Units',
+               'Decoder Layer 1 Units',
+               'Decoder Layer 2 Units',
+               'Train Error',
+               'Validation Error',
+               'Test Error'
+               ]
+    df = pd.DataFrame(columns=columns)
+
+    for t in ts :
+
+        end_window_size = t
+
+        for enc in _enc_units :
+            for dec in _dec_units :
+                res_dict = {}
+                model_obj = encoder_decoder_model()
+                model_obj.set_hyperparams(
+                    exg_dimension,
+                    end_window_size,
+                    encoder_layer_units,
+                    decoder_layer_units,
+                    epochs,
+                    lstm_time_steps,
+                    batch_size
+                )
+
+                model_obj.build_model()
+                train_mse = model_obj.train_model()
+                val_mse = model_obj.val_model()
+                test_mse = model_obj.test_model()
+
+                res_dict[columns[0]] = t
+
+                res_dict[columns[1]] = enc[0]
+                res_dict[columns[2]] = enc[1]
+                res_dict[columns[3]] = dec[0]
+                res_dict[columns[4]] = dec[1]
+
+                res_dict[columns[5]] = train_mse
+                res_dict[columns[6]] = val_mse
+                res_dict[columns[7]] = test_mse
+
+    df.to_csv('model_5_op.csv')
+
+experiment()
